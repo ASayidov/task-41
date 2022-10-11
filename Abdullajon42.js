1eeea9068c48f2ddd096a295e39b60143c01a224
ASayidov <abdullajonsayidov@gmail.com>
2022-10-11 22:39:28 +0500

    finished task 43

--- Abdullajon42.js
+++ Abdullajon42.js
@@ -1,61 +1,52 @@
-// function numberSplit(n) {
-//   let x = [];
-//   let y = n % 2;
-//   let z = n / 2;
-//   if (y == 0) {
-//     x.push(z, z);
-//   }
-//   if (y !== 0) {
-//     x.push(z - 0.5, z + 0.5);
-//   }
-//   return x;
-// }
+function numberSplit(n) {
+  let x = [];
+  let y = n % 2;
+  let z = n / 2;
+  if (y == 0) {
+    x.push(z, z);
+  }
+  if (y !== 0) {
+    x.push(z - 0.5, z + 0.5);
+  }
+  return x;
+}
 
-// function addNums(nums) {
-//   let x = nums.split(',')
-//   let res =0;
-//   x.forEach(element => {
-//     let z = parseInt(element)
-//     res +=z
-//   });
-//   return res
+function addNums(nums) {
+  let x = nums.split(',')
+  let res =0;
+  x.forEach(element => {
+    let z = parseInt(element)
+    res +=z
+  });
+  return res
 
-// }
+}
 
-// function filterArray(arr) {
-//   return arr.filter(x => typeof x !== 'string');
-// }
-
-
-// function maxTotal(nums) {
-// let sort = nums.sort((a,b)=>a-b)
-// let sum =0;
-// for (let i = 5; i < sort.length; i++) {
-//   sum += sort[i]
-// }
-// return sum
-// }
+function filterArray(arr) {
+  return arr.filter(x => typeof x !== 'string');
+}
 
 
-// sliceSum([1, 3, 2], 2) ➞ 4
+function maxTotal(nums) {
+let sort = nums.sort((a,b)=>a-b)
+let sum =0;
+for (let i = 5; i < sort.length; i++) {
+  sum += sort[i]
+}
+return sum
+}
 
-// sliceSum([4, 2, 5, 7], 4) ➞ 18
-
-// sliceSum([3, 6, 2], 0) ➞ 0
 
 
-// function sliceSum(arr, n) {
-// 	let newArr = arr.slice(0,n)
-//   let count =0;
-//   for (let i = 0; i < newArr.length; i++) {
-//     count+=newArr[i]
-//   }
-// return count
-// }
+function sliceSum(arr, n) {
+	let newArr = arr.slice(0,n)
+  let count =0;
+  for (let i = 0; i < newArr.length; i++) {
+    count+=newArr[i]
+  }
+return count
+}
 
-// cardHide("1234123456785678") ➞ "************5678"
-
-let card = '1234123456785678'
 
 function cardHide(card) {
 let turt = card.slice(-4)
@@ -66,8 +57,6 @@ let result=""
 for (let i = 0; i < newArr.length; i++) {
  result += newArr[i]='*'
 }
-return result
-}
 
-let res = cardHide(card)
-console.log(res);
+return result.concat(turt)
+}
